8fad0ab01ba0b6d0b6e41d82f14d1153429f7e09
LetitiaHo <lho1@swarthmore.edu>
2018-06-28 23:32:33 +0100

    updated guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,29 +1,29 @@
-function guessinggame {
-  answer=0
-  files=$(getfilenumber)
-
-  while [[ $answer != $files ]]
-  do 
-
-    echo "Guess the number of files in the current directory"
-    read answer 
-
-    if [[ $answer -gt $files ]] 
-    then
-       echo "Your guess was too high"
-    elif [[ $answer -lt $files ]] 
-    then
-      echo "Your guess was too low"
-    fi
-
-  done
-
-  echo "You are correct, there are $files files in the current directory"
-}
-
-
-
 function getfilenumber {
   local files=$(ls -l . | egrep -c '^-')
   echo $files
 }
+
+answer=0
+files=$(getfilenumber)
+
+while [[ $answer != $files ]]
+do 
+
+  echo "Guess the number of files in the current directory"
+  read answer 
+
+  if [[ $answer -gt $files ]] 
+  then
+    echo "Your guess was too high"
+  elif [[ $answer -lt $files ]] 
+  then
+    echo "Your guess was too low"
+  fi
+
+done
+
+echo "You are correct, there are $files files in the current directory"
+
+
+
+
